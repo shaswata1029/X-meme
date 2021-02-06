aa6d07ee96fbed832b563f396ee922590534e7ee
shaswata1029 <shaswata1029@gmail.com>
2021-02-06 15:42:26 +0530

    updated some rendering calls in the server

--- backend/controllers/homecontroller.js
+++ backend/controllers/homecontroller.js
@@ -19,7 +19,7 @@ module.exports.postMeme = function(req, res) {
 
     Meme.findOne({ url: req.body.url }, function(err, meme) {
         if (err) {
-            console.log('error in creating the meme  ');
+            console.log('error in creating the meme');
             return res.end('Error 404');
         }
         if (meme) {
@@ -40,30 +40,45 @@ module.exports.postMeme = function(req, res) {
 }
 
 module.exports.editMeme = function(req, res) {
-    const url = req.body.url;
-    const caption = req.body.caption;
-    const id = req.params.id;
-    let update = {};
-    if (caption == "" && url == "") {
-        return res.redirect('back');
-    }
-
-    if (url == "") {
-        update.caption = caption;
-    } else if (caption == "") {
-        update.url = url;
-    } else {
-        update.caption = caption;
-        update.url = url;
-    }
 
 
-    Meme.findByIdAndUpdate(id, update, { new: true }, function(err, meme) {
+    Meme.findOne({ url: req.body.url }, function(err, meme) {
         if (err) {
-            console.log('error in updating the user');
-            return res.end('error 404');
+            console.log('error in updating the meme');
+            return res.end('Error 404');
         }
-        console.log('meme updated');
-        return res.redirect('back');
+        if (meme) {
+            console.log('meme alraedy present');
+            return res.end('Error 409, Meme already present');
+        } else {
+            const url = req.body.url;
+            const caption = req.body.caption;
+            const id = req.params.id;
+            let update = {};
+            if (caption == "" && url == "") {
+                return res.redirect('back');
+            }
+
+            if (url == "") {
+                update.caption = caption;
+            } else if (caption == "") {
+                update.url = url;
+            } else {
+                update.caption = caption;
+                update.url = url;
+            }
+
+
+            Meme.findByIdAndUpdate(id, update, { new: true }, function(err, meme) {
+                if (err) {
+                    console.log('error in updating the meme');
+                    return res.end('error 404');
+                }
+                console.log('meme updated');
+                return res.redirect('back');
+            });
+        }
     });
+
+
 }
