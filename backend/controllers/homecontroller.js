dd92af8e429347ca58ab60af9777ebfae029d88c
shaswata1029 <shaswata1029@gmail.com>
2021-02-06 11:35:26 +0530

    added the edit section of the meme functionality

--- backend/controllers/homecontroller.js
+++ backend/controllers/homecontroller.js
@@ -16,7 +16,7 @@ module.exports.home = function(req, res) {
 }
 
 module.exports.postMeme = function(req, res) {
-    Meme.create({ name: req.body.name, url: req.body.url, caption: req.body.caption }, function(err, user) {
+    Meme.create({ name: req.body.name, url: req.body.url, caption: req.body.caption }, function(err, meme) {
         if (err) {
             console.log('error in creating the user');
             return res.end('error 404');
@@ -25,3 +25,41 @@ module.exports.postMeme = function(req, res) {
         return res.redirect('back');
     });
 }
+
+module.exports.editMeme = function(req, res) {
+        const url = req.body.url;
+        const caption = req.body.caption;
+        const id = req.params.id;
+        let update = {};
+        if (caption == "" && url == "") {
+            return res.redirect('back');
+        }
+
+        if (url == "") {
+            update.caption = caption;
+        } else if (caption == "") {
+            update.url = url;
+        } else {
+            update.caption = caption;
+            update.url = url;
+        }
+
+        // Meme.findOneAndUpdate(filter, update, { new: true }, function(err, meme) {
+        //     if (err) {
+        //         console.log('error in updating the user');
+        //         return res.end('error 404');
+        //     }
+        //     console.log('meme updated');
+        //     return res.redirect('back');
+        // });
+
+        Meme.findByIdAndUpdate(id, update, { new: true }, function(err, meme) {
+            if (err) {
+                console.log('error in updating the user');
+                return res.end('error 404');
+            }
+            console.log('meme updated');
+            return res.redirect('back');
+        });
+    }
+    //
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,5 +6,6 @@ const homeController = require('../controllers/homecontroller.js');
 router.get('/', homeController.home);
 router.use('/memes', require('../memes/index'));
 router.post('/post_memes', homeController.postMeme);
+router.post('/edit_memes/:id', homeController.editMeme);
 
 module.exports = router;
