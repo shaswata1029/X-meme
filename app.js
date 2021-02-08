1e4b83bde801254cb6bd0bcd76e392e06d9f1f4a
shaswata1029 <shaswata1029@gmail.com>
2021-02-08 17:41:43 +0530

    added config variablrs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv');
 
 const app = express();
-const port = 8081;
-
+// const port = 8081;
+dotenv.config();
 
 // database connection
 const db = require('./config/mongoose');
@@ -21,10 +21,10 @@ app.use('/', require('./backend/routes/index.js'));
 app.set('view engine', 'ejs');
 app.set('views', './frontend/views');
 
-app.listen(port, function(err) {
+app.listen(process.env.PORT, '0.0.0.0', function(err) {
     if (err) {
         console.log(`Error is ${err} on port ${port}`);
         return;
     }
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
