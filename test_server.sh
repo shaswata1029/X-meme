f2f2fd3602f9b680e6915279f27d482b49e17c6a
Shaswata Raha <shaswata1029@gmail.com>
2021-02-10 21:54:05 +0530

    test server

--- test_server.sh
+++ test_server.sh
@@ -39,11 +39,7 @@ curl --location --request GET 'http://localhost:8081/memes'
 
 # Execute the POST /memes endpoint using curl
 
-curl --location --request POST 'http://localhost:8081/memes' \ --header 'Content-Type: application/json'\ --data-raw '{
-"name": "xyz",
-"url": "abc.com",
-"caption": "This is a meme"
-}'
+curl --location --request POST 'http://localhost:8081/memes' \ --header 'Content-Type: application/json' \ --data-raw '{"name": "xyz","url": "abc.com","caption": "This is a meme"}'
 
 
 # Execute the GET /memes endpoint using curl
