451ad71ab5864b791467db9bb1f3e48ea2a31cdc
Shaswata Raha <shaswata1029@gmail.com>
2021-02-10 11:25:27 +0530

    added some documentation

--- app.js
+++ app.js
@@ -25,21 +25,22 @@ app.use('/', require('./backend/routes/index.js'));
 
 
 
-app.listen(process.env.PORT, '0.0.0.0', function(err) {
-    if (err) {
-        console.log(`Error is ${err} on port ${port}`);
-        return;
-    }
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
-// setting up the server with express for deployment
-
-// app.listen(port, function(err) {
+// app.listen(process.env.PORT, '0.0.0.0', function(err) {
 //     if (err) {
 //         console.log(`Error is ${err} on port ${port}`);
 //         return;
 //     }
-//     console.log(`Server is running on port ${port}`);
+//     console.log(`Server is running on port ${process.env.PORT}`);
 // });
+
+// setting up the server with express for deployment
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(`Error is ${err} on port ${port}`);
+        return;
+    }
+    console.log(`Server is running on port ${port}`);
+});
 
 // setting up the server for local testing
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 // requiring mongoose
 
-mongoose.connect('mongodb+srv://shaswata1029:sssbn1029@cluster0.1wybo.mongodb.net/X-meme?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-// // connect to the database
+// mongoose.connect('mongodb+srv://shaswata1029:sssbn1029@cluster0.1wybo.mongodb.net/X-meme?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// //  connect to the database
 
 // mongoose.connect('mongodb+srv://shaswata1029:sssbn1029@cluster0.1wybo.mongodb.net/X-meme_db?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 // connect to the database
+
+// for public deployment
+
+mongoose.connect('mongodb://localhost/X-meme_db', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// for local testing 
+//  connect to the database
 
 
 //  acquire the connection to check if it is succesful
